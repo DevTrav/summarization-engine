687784820f28c6070d3b7f58ac8b88d6e3cd6461
Trav La Ville <18622201+DevTrav@users.noreply.github.com>
2025-03-07 13:35:26 -0600

    Added cron job to check service

--- summarizer/monitor/check.go
+++ summarizer/monitor/check.go
@@ -4,7 +4,9 @@ import (
 	"context"
 
 	"encore.app/site"
+	"encore.dev/cron"
 	"encore.dev/storage/sqldb"
+	"golang.org/x/sync/errgroup"
 )
 
 // Check checks if a site is up
@@ -15,15 +17,39 @@ func Check(ctx context.Context, siteID int) error {
 	if err != nil {
 		return err
 	}
+	return check(ctx, site)
+}
+func check(ctx context.Context, site *site.Site) error {
 	result, err := Ping(ctx, site.URL)
 	if err != nil {
 		return err
 	}
-	_, err = db.Exec(ctx,
-		`INSERT INTO checks (site_id, up, checked_at)
-		VALUES ($1, $2, NOW())`,
-		site.ID, result.Up)
+	_, err = db.Exec(ctx, `
+		INSERT INTO checks (site_id, up, checked_at)
+		VALUES ($1, $2, NOW())
+	`, site.ID, result.Up)
 	return err
+}
+
+// CheckAll checks all sites and records the results
+//
+//encore:api public method=POST path=/checkall
+func CheckAll(ctx context.Context) error {
+	// Get all the tracked sites
+	resp, err := site.List(ctx)
+	if err != nil {
+		return err
+	}
+	// Check up to 8 sites concurrently.
+	g, ctx := errgroup.WithContext(ctx)
+	g.SetLimit(8)
+	for _, site := range resp.Sites {
+		site := site // capture the loop variable for closure
+		g.Go(func() error {
+			return check(ctx, site)
+		})
+	}
+	return g.Wait()
 }
 
 // Define a DB named monitor, using database migrations
@@ -31,4 +57,11 @@ func Check(ctx context.Context, siteID int) error {
 // Encore automatically provisions, migrates and connects to the db.
 var db = sqldb.NewDatabase("monitor", sqldb.DatabaseConfig{
 	Migrations: "./migrations",
+})
+
+// Check all tracked sites every 1 hour.
+var _ = cron.NewJob("check-all", cron.JobConfig{
+	Title:    "Check all sites",
+	Endpoint: CheckAll,
+	Every:    1 * cron.Hour,
 })
